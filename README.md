674196779d2abfa657bf69d04c67a1f1c6dd9c7a
azhon <zhongsy@vvsmart.com>
2018-08-13 17:02:56 +0800

    爬取校花图片

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Python
 python学习的相关代码
+## 爬虫依赖了一些第三方库，需要自己安装
+#### 已知使用到的第三方库
+- BeautifulSoup
+- pymysql
+- requests
 
 #### [juejin.py](https://github.com/azhon/Python/blob/master/juejin.py) —— 爬取掘金Android分类的文章
 
 #### [pymsql](https://github.com/azhon/Python/tree/master/pymysql) —— [操作MySQL](https://blog.csdn.net/a_zhon/article/details/81630825)
+
+#### [girl.py](https://github.com/azhon/Python/tree/master/girl.py) —— 爬取2014年榜单校花图片
---
+++ girl.py
@@ -0,0 +1,35 @@
+# 爬取校花图片
+
+from urllib import request
+from bs4 import BeautifulSoup as bs
+import os
+import requests
+
+dir = "C:\\Users\\Administrator\\Desktop\\xiaohua"
+url = "http://www.xiaohuar.com/2014.html"
+imgStart = "http://www.xiaohuar.com/"
+
+resp = request.Request(url=url)
+resp = request.urlopen(resp)
+# 网页返回的数据
+htmlCode = resp.read().decode("gbk")
+
+soup = bs(htmlCode, "html.parser")
+allGirl = soup.find_all("div", class_="img")
+# 创建文件夹
+exists = os.path.exists(dir)
+if not exists:
+    os.makedirs(dir)
+
+for girl in allGirl:
+    content = girl.find_all("img")[0]
+    name = content.get("alt")
+    imgUrl = content.get("src")
+    # 判断是不是已http开头的图片地址，不是的话拼接全路径
+    if not imgUrl.startswith('http://'):
+        imgUrl = imgStart + imgUrl
+    # 下载图片
+    a = requests.get(imgUrl)
+    with open(r'%s\%s.jpg' % (dir, name), 'wb') as f:
+        f.write(a.content)
+        print("%s====下载完成..." % name)
